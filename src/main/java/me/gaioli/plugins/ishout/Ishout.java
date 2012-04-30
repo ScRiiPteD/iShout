62aabde1124808faa0dbfc41b424c45090a2caf3
Tjs238 <tjs238@gmail.com>
2012-04-29 23:42:09 -0400

    Init Commit

---
+++ src/main/java/me/gaioli/plugins/ishout/Ishout.java
@@ -0,0 +1,22 @@
+package me.gaioli.plugins.ishout;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Ishout extends JavaPlugin implements Listener {
+    public void onDisable() {
+        // TODO: Place any custom disable code here.
+    }
+
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        event.getPlayer().sendMessage("Welcome, " + event.getPlayer().getDisplayName() + "!");
+    }
+}
+
